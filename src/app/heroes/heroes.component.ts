0d97ad1d64831543e1807ce03a05aba1861a1f99
Florian CHATELUS <florian.chatelus@yahoo.fr>
2020-11-01 00:32:35 +0100

    [FRONT][TUTO] adding router

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,20 +9,11 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero  = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
   heroes: Hero[];
   selectedHero: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
