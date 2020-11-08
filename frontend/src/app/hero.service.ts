ca78e2632dbcb6a2da21b80e318749f9cc75094b
Florian CHATELUS <florian.chatelus@yahoo.fr>
2020-11-08 22:32:33 +0100

    [FRONT] update front to fetch heroes from the backend

--- frontend/src/app/hero.service.ts
+++ frontend/src/app/hero.service.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of} from 'rxjs';
+import { from, Observable, of} from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+  
+  private heroesUrl = 'http://localhost:3000/api/heroes';
+
+  private log(message: string): void {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetchted heroes');
-    return of(HEROES);
+    this.log(`fetch heroes`);
+    return this.http.get<Hero[]>(this.heroesUrl);
   }
 
   getHero(id: number): Observable<Hero> {
-    this.messageService.add(`HeroService: fetch hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+
+    this.log(`fetch hero id=${id}`);
+    return this.http.get<Hero>(this.heroesUrl + `/${id}`);
   }
 }
