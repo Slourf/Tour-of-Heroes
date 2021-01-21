46f402e999399108521f6e407e1a7303d229b7a2
Florian CHATELUS <florian.chatelus@yahoo.fr>
2021-01-21 14:42:47 +0100

    [BACK] Tell if the server is running on /

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
   });
-
+app.use('/', (req, res) => {
+    res.send("The server is up!")
+})
 app.use('/api', api);
 
 app.use((req, res, next) => {
