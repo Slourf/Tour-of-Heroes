1fdcecaeca569ba46bb38e4df4ac9148a86249b7
Florian CHATELUS <florian.chatelus@yahoo.fr>
2021-04-17 22:07:03 +0200

    [BACK] update middleware to communicate properly

--- backend/src/api/index.ts
+++ backend/src/api/index.ts
@@ -8,7 +8,6 @@ export const router: Router = express.Router();
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
   console.log("Time: ", Date.now());
-  res.status(200).json({ health: "API OK" });
   next();
 });
 
